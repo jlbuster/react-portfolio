a1c6731b7998cc351c0aa9ae4e78b0ec225c2a83
John Blume <jlblumework@gmail.com>
2020-07-07 18:17:54 -0400

    Updates

---
+++ precache-manifest.2cb6290f4805287ce08820a390f8e620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7489bf044dbc2fd2635510ac444b5ff2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b993d72126fd83d1b69f",
+    "url": "/react-portfolio/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "c6b4a2c42cb1f1f27477",
+    "url": "/react-portfolio/static/js/2.38fc6a77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.38fc6a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b993d72126fd83d1b69f",
+    "url": "/react-portfolio/static/js/main.8fce7e19.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6e62074142ec69e37b7bb82f0bbe1505",
+    "url": "/react-portfolio/static/media/profile_pic.6e620741.jpg"
+  }
+]);
